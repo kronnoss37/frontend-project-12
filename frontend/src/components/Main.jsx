55e8d9f31bf3e8aed5e49f0f39bd4885151877ec
kronnoss <kronnoss.it@gmail.com>
2025-10-22 22:58:36 +0300

    Create component Main.jsx with main logic that manages channels and chat

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Channels from './Channels'
+import Chat from './Chat';
+import { getChannels, setCurrentChannel } from '../store/slices/channelsSlice';
+import { getMessages } from '../store/slices/messagesSlice';
+
+const Main = () => {
+
+  const dispatch = useDispatch();
+
+  const userData = useSelector((state) => state.authData.user);
+  const channels = useSelector((state) => state.channelsData.channels);
+  const currentChannel = useSelector((state) => state.channelsData.currentChannel);
+
+  const messages = useSelector((state) => state.messagesData.messages);
+
+  useEffect(() => {
+    const token = userData?.token;
+    if (token) {
+      dispatch(getChannels(token));
+      dispatch(getMessages(token));
+    }
+  }, []);
+
+  const changeChannel = (newChannel) => dispatch(setCurrentChannel(newChannel));
+
+  return (
+    <>
+      <Channels channels={channels} currentChannel={currentChannel} changeChannel={changeChannel} />
+      <Chat currentChannel={currentChannel} messages={messages} />
+    </>
+  );
+}
+
+export default Main
