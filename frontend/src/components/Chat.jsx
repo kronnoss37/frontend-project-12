41307ab99f3865a82ef197134bc024991dfc68a6
kronnoss <kronnoss.it@gmail.com>
2025-10-22 23:05:00 +0300

    Create components Chat.jsx, Messages.jsx and ChatForm.jsx with main logic

---
+++ frontend/src/components/Chat.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Col } from 'react-bootstrap';
+
+import Messages from './Messages';
+import ChatForm from './ChatForm';
+
+
+const Chat = ({ currentChannel, messages }) => {
+  const channelMessages = messages?.filter(({ channelId }) => channelId === currentChannel.id);
+  const messagesCount = channelMessages?.length;
+
+  return (
+    <Col className='p-0 h-100'>
+      <div className="d-flex flex-column h-100">
+        {currentChannel && (
+          <div className='bg-light mb-4 p-3 shadow-sm small'>
+            <p className='m-0'>
+              <b>{`# ${currentChannel?.name}`}</b>
+            </p>
+            <span className='text-muted'>{messagesCount} сообщений</span>
+            {/** after i18n - 1 сообщение, 2-4 сообщения, 5-.. сообщений */}
+          </div>
+        )}
+        <Messages channelMessages={channelMessages} />
+        <ChatForm />
+      </div>
+    </Col>
+  );
+}
+
+export default Chat
---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Messages = ({ channelMessages }) => {
+  return (
+    <div id='messages-box' className='chat-messages overflow-auto px-5'>
+      {channelMessages?.map((message) => (
+        <div key={message.id} className='text-break mb-2'>
+          <b>{`${message.username}:`}</b>
+          {message.body}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Messages
