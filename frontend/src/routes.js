3707e0712b4a3c2991df46ae3f6b4e4ee96311e1
kronnoss <kronnoss.it@gmail.com>
2025-10-22 23:06:25 +0300

    Update file routes.js for different requests

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,8 @@ const apiPath = '/api/v1'
 
 const routes = {
   loginPath: () => [apiPath, 'login'].join('/'),
-}
+  channelsPath: (id = '') => [apiPath, `channels`, id].filter(Boolean).join('/'),
+  messagesPath: (id = '') => [apiPath, `messages`, id].filter(Boolean).join('/'),
+};
 
 export default routes
