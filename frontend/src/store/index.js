32581644469e9c61bf11b4ebb0f4dc711bc4c2e9
kronnoss <kronnoss.it@gmail.com>
2025-10-28 00:10:51 +0300

    Add new slice notificationsSlice for working with notifications

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,11 +3,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import channelsReducer from './slices/channelsSlice'
 import messagesReducer from './slices/messagesSlice'
+import notificationsReducer from './slices/notificationsSlice'
+
 
 export default configureStore({
   reducer: {
     authData: authReducer,
     channelsData: channelsReducer,
     messagesData: messagesReducer,
+    notifications: notificationsReducer,
   },
-})
+});
---
+++ frontend/src/store/slices/notificationsSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  notification: {
+    type: null, // error | success | null
+    path: ''
+  }
+}
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Возможно, стоит сделать массив исключающих запросов
+    builder
+      .addMatcher(
+        (action) => action.type.endsWith('/pending'),
+        (state) => {
+          state.type = null;
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/fulfilled'),
+        (state, action) => {
+          console.log('action.type', action.type);
+          console.log('notify fulfilled', action.payload);
+          const data = action.payload;
+          if (data?.notificationPath) {
+            state.notification = { type: 'success', path: data.notificationPath };
+          }
+        }
+      )
+      .addMatcher(
+        (action) => action.type.endsWith('/rejected'),
+        (state, action) => {
+          console.log('action.type', action.type);
+          console.log('notify rejected', action.payload);
+          const data = action.payload;
+          if (data?.notificationPath) {
+            state.notification = { type: 'error', path: data.notificationPath };
+          }
+        }
+      );
+  }
+});
+
+export default notificationsSlice.reducer
