0ddf5eea9d0b1936f3dd2997dbfe0b0a404740f1
kronnoss <kronnoss.it@gmail.com>
2025-10-27 23:08:06 +0300

    Add loading custom button to the project

---
+++ frontend/src/buttons/LoadingButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const LoadingButton = ({ isVisuallyHidden = false, variant = 'primary', spinnerColor = ''}) => {
+  return (
+    <Button variant={variant} type='button' disabled>
+      <span className={`spinner-border spinner-border-sm ${spinnerColor}`} aria-hidden='true'></span>
+      {' '}
+      <span role='status' className={isVisuallyHidden ? 'visually-hidden' : ''}>Загрузка...</span>
+    </Button>
+  );
+}
+
+export default LoadingButton
