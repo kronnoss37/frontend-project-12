371dca04537cf903500d53a4c7176884c8791d89
kronnoss <kronnoss.it@gmail.com>
2025-11-01 18:50:14 +0300

    Fix error in the file Channels.jsx

--- frontend/src/components/Chat/Channels/Channels.jsx
+++ frontend/src/components/Chat/Channels/Channels.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, ButtonGroup } from 'react-bootstrap'
+import { Col, Button } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 
 import Channel from './Channel'
@@ -11,7 +11,7 @@ const Channels = ({ channels, currentChannel, changeChannel, handleOpenModal }) 
     <Col xs={4} md={2} className="border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>{t('channels.title')}</b>
-        <ButtonGroup vertical className="p-0 text-primary btn" onClick={() => handleOpenModal({ type: 'add' })}>
+        <Button variant='ligth' className="p-0 text-primary btn-group-vertical" onClick={() => handleOpenModal({ type: 'add' })}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
@@ -24,7 +24,7 @@ const Channels = ({ channels, currentChannel, changeChannel, handleOpenModal }) 
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
           </svg>
           <span className="visually-hidden">{t('channels.addButton')}</span>
-        </ButtonGroup>
+        </Button>
       </div>
 
       {channels?.length > 0 && (
